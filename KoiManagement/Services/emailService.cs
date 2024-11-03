eba87af233b0ec904690b8175523de2eef66025c
vanhan812 <hannv2246@ut.edu.vn>
2024-11-03 18:12:04 +0700

    update service

---
+++ KoiManagement/Services/emailService.cs
@@ -0,0 +1,10 @@
+﻿namespace KoiManagement.WebApplication.Services
+{
+    public class EmailService
+    {
+        public void SendEmail(string to, string subject, string body)
+        {
+            Console.WriteLine($"Gửi email đến {to} với tiêu đề: {subject} và nội dung: {body}");
+        }
+    }
+}
---
+++ KoiManagement/Services/petService.cs
@@ -0,0 +1,18 @@
+﻿namespace KoiManagement.WebApplication.Models
+{
+    public class PetService
+    {
+        public int Id { get; set; }
+        public string? PetName { get; set; } 
+        public string? OwnerName { get; set; } 
+        public string? ServiceType { get; set; } 
+
+        public string? DoctorName {  get; set; }
+        public int? Rating { get; set; }
+        public string? Feedback { get; set; } = string.Empty;
+    public DateTime ServiceDate { get; set; }
+
+   public DateTime ServiceDate { get; set; }
+
+    }
+}
